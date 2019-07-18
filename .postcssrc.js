43120d66248ce773feed5b317e18c94ba873bce1
Carl Törnqvist <carl@tornqv.ist>
2019-07-18 18:28:05 +0200

    Embed svg in css

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,7 +3,14 @@ module.exports = config
 function config (ctx) {
   var plugins = [require('postcss-focus-visible')({ preserve: false })]
   if (ctx.env !== 'development') {
-    plugins.push(require('postcss-custom-properties'))
+    plugins.push(
+      require('postcss-custom-properties'),
+      require('postcss-url')({
+        filter: /\.svg$/,
+        url: 'inline',
+        maxSize: Infinity
+      })
+    )
   }
 
   return { plugins }
